6ddcb11777578789b832e61b286080b6625af7ca
JangXa <m.sahin@tum.de>
2017-10-27 18:31:34 +0200

    Update package.py

--- package.py
+++ package.py
@@ -14,16 +14,4 @@ for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
 
 
 
-#filename = "hello.java"
-#file = open(filename, "r")
-#lines = file.readlines()
-#file.close()
-#file = open(filename, "w")
-#for line in lines:
-#    if "package" not in line:
-#        file.write(line)
-#
-#file.close()
-
-
 
