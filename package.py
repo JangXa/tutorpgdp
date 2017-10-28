9eb49b4cc4e0894d5c1d9a1aeed4d0a4c73be739
Muhammed Sahin <m.sahin@tum.de>
2017-10-28 18:32:41 +0200

    only delete first line now

--- package.py
+++ package.py
@@ -7,9 +7,14 @@ for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
             lines = file.readlines()
             file.close()
             file = open(name, "w")
+            
+            i = 0 
             for line in lines:
-                if "package" not in line:
+                if "package" not in line or i != 0:
                     file.write(line)
+                    print("a")
+                i += 1
+                print(i)
             file.close()
 
 
