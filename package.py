05d4cfbb2ec1ac857df6b836cc828df46cac7737
Muhammed Sahin <m.sahin@tum.de>
2017-10-28 20:55:26 +0200

    delete only if package is at top

--- package.py
+++ package.py
@@ -10,6 +10,8 @@ for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
             
             i = 0 
             for line in lines:
+                if not line.strip():
+                    continue
                 if "package" not in line or i != 0:
                     file.write(line)
                 i += 1
